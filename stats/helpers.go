9588913998ad4894a87954582cadc7b5df381c2d
Lior Nabat <lior.nabat@gmail.com>
2018-10-14 18:04:34 +0300

    add helpers

---
+++ stats/helpers.go
@@ -0,0 +1,49 @@
+package stats
+
+import "time"
+
+const (
+	KindPublish            = "publish"
+	KindPublishPersistence = "publish_persistence"
+)
+
+func ReportPublish(node, clientID, channel string, msgCount, msgSize float64) {
+	key := GetKey(node, clientID, channel, "", KindPublish, "")
+	key.Record(Item{
+		MsgCount:   msgCount,
+		MsgSize:    msgSize,
+		CacheHit:   0,
+		CacheMiss:  0,
+		Errors:     0,
+		Latency:    0,
+		LastUpdate: time.Now().UTC().UnixNano(),
+	})
+}
+
+func ReportPublishError(node, clientID, channel string) {
+	key := GetKey(node, clientID, channel, "", KKindPublish, "")
+	key.Record(Item{
+		Errors:     1,
+		LastUpdate: time.Now().UTC().UnixNano(),
+	})
+}
+func ReportPublishPersistence(node, clientID, channel string, msgCount, msgSize float64) {
+	key := GetKey(node, clientID, channel, "", KindPublishPersistence, "")
+	key.Record(Item{
+		MsgCount:   msgCount,
+		MsgSize:    msgSize,
+		CacheHit:   0,
+		CacheMiss:  0,
+		Errors:     0,
+		Latency:    0,
+		LastUpdate: time.Now().UTC().UnixNano(),
+	})
+}
+
+func ReportPublishPersistenceError(node, clientID, channel string) {
+	key := GetKey(node, clientID, channel, "", KindPublishPersistence, "")
+	key.Record(Item{
+		Errors:     1,
+		LastUpdate: time.Now().UTC().UnixNano(),
+	})
+}
